8bd91e902725146d14c87d5161502f901bffcaa0
James Shaughnessy <jws325@cornell.edu>
2015-04-01 22:11:17 -0400

    Added recursive BST searches. Not a very good way to do BFS, but it works.

--- interview_questions.py
+++ interview_questions.py
@@ -79,7 +79,7 @@ def test_ll():
 
 def test_fibonacci():
 
-    memoized_vals = {0: 0, 1: 1}
+    memoized_vals = {}
 
     def get_memoized(n):
         if n in memoized_vals:
@@ -119,4 +119,91 @@ def test_fibonacci():
     else:
         raise RuntimeError('The fibonacci function failed to throw an error for the negative index')
 
-test_fibonacci()
+# 4. Implement pre-order, in-order, post-order, and breadth-first recursive traversals of a binary tree
+
+def test_binary_tree_recursive_traversals():
+    
+    class BinaryTree(object):
+        def __init__(self, value, left_child=None, right_child=None):
+            self.value = value
+            self.left_child = left_child
+            self.right_child = right_child
+
+        def get_val(self):
+            return self.value
+
+        def get_left_child(self):
+            return self.left_child
+
+        def get_right_child(self):
+            return self.right_child
+
+        def set_left_child(self, left_child):
+            self.left_child = left_child
+
+        def set_right_child(self, right_child):
+            self.right_child = right_child
+
+    # make the tree
+    """
+        1
+       2 3
+      4   5
+    """
+
+    head = BinaryTree(
+        1,
+        left_child=BinaryTree(
+            2, left_child=BinaryTree(4)
+        ),
+        right_child=BinaryTree(
+            3, right_child=BinaryTree(5)
+        )
+    )
+
+    def pre_order_depth_first_search(node, q):
+        if node:
+            q.append(node.value)
+            q.extend(pre_order_depth_first_search(node.get_left_child(), []))
+            q.extend(pre_order_depth_first_search(node.get_right_child(), []))
+
+        return q
+
+    assert pre_order_depth_first_search(head, []) == [1, 2, 4, 3, 5]
+
+    def in_order_depth_first_search(node, q):
+        if node:
+            q.extend(in_order_depth_first_search(node.get_left_child(), []))
+            q.append(node.value)
+            q.extend(in_order_depth_first_search(node.get_right_child(), []))
+
+        return q
+
+    assert in_order_depth_first_search(head, []) == [4, 2, 1, 3, 5]
+
+    def post_order_depth_first_search(node, q):
+        if node:
+            q.extend(post_order_depth_first_search(node.get_left_child(), []))
+            q.extend(post_order_depth_first_search(node.get_right_child(), []))
+            q.append(node.value)
+
+        return q
+
+    assert post_order_depth_first_search(head, []) == [4, 2, 5, 3, 1]
+
+    def breadth_first_search(node, q):
+
+        if node:
+            q.append(node.value)
+            left_children = breadth_first_search(node.get_left_child(), [])
+            right_children = breadth_first_search(node.get_right_child(), [])
+
+            max_child_level = max(len(left_children), len(right_children))
+
+            for x in range(max_child_level):
+                for children in (left_children, right_children):
+                    if len(children) > x:
+                        q.append(children[x])
+        return q
+
+    assert breadth_first_search(head, []) == [1, 2, 3, 4, 5]
